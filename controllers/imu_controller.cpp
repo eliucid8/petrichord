d90c97e72399608e4e2cfb90395e15c8bd3e0384
Sam Ledden <44735694+samledden04@users.noreply.github.com>
2025-11-10 17:36:56 -0500

    Added IMU/I2C support to PICO

---
+++ controllers/imu_controller.cpp
@@ -0,0 +1,62 @@
+#include "imu_controller.h"
+#include <cstdio>
+
+IMU_Controller::IMU_Controller() 
+{
+}
+
+bool IMU_Controller::init(i2c_inst_t *i2c_port) {
+    s8 err = bno055_pico_init(&bno_, i2c_port, BNO055_I2C_ADDR1);
+
+    if(err) {
+        err = bno055_pico_init(&bno_, i2c_port, BNO055_I2C_ADDR2);
+    }
+
+    if(err) {
+        printf("IMU_Controller: BNO055 initialization failiure.\n");
+        return false;
+    }
+
+    err = bno055_set_power_mode(BNO055_POWER_MODE_NORMAL);
+
+    if(err) {
+        printf("IMU_Controller: power mode request failed.\n");
+        return false;
+    }
+
+    err = bno055_set_operation_mode(BNO055_OPERATION_MODE_ACCGYRO);
+    if(err) {
+        printf("IMU_Controller: operation mode request failed.\n");
+        return false;
+    }
+
+    printf("IMU_Controller: initialized succesfully.\n");
+
+    return true;
+}
+
+bool IMU_Controller::readGravityVector(struct imu_gravity_vector *dst) {
+
+    struct bno055_accel_float_t accelData;
+    s8 err = bno055_convert_float_accel_xyz_msq(&accelData);
+
+    if(err) {
+        printf("IMU_Controller: bno055 data read failiure.\n");
+        return false;
+    }
+
+    dst->x = accelData.x;
+    dst->y = accelData.y;
+    dst->z = accelData.z;
+
+    return true;
+}
+
+void IMU_Controller::debugPrint() {
+    struct imu_gravity_vector vec;
+
+    if(readGravityVector(&vec)) {
+        printf("IMU_Controller: X: %.2f, Y: %.2f, Z: %.2f\n", vec.x, vec.y, vec.z);
+    }
+    
+}
--- hello_matrix.cpp
+++ hello_matrix.cpp
@@ -6,10 +6,12 @@
 #include "hardware/pio.h"
 #include "hardware/timer.h"
 #include "hardware/uart.h"
+#include "hardware/i2c.h"
 
 #include "input/key_matrix.h"
 #include "input/fake_strum_irq.h"
 #include "controllers/chord_controller.h"
+#include "controllers/imu_controller.h"
 #include "output/midi_messenger.h"
 
 #include "blink.pio.h"
@@ -18,16 +20,30 @@ static ChordController* g_chord_controller = nullptr;
 
 #define LED_PIN 15
 
+// I2C Constants
+#define I2C_CHANNEL_ONE i2c1
+#define IMU_SDA 26
+#define IMU_SCL 27
+
 int main()
 {
     stdio_init_all();
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
+    // gpio_put(LED_PIN, 1);
     
     uart_init(uart0, 31250);
     gpio_set_function(0, GPIO_FUNC_UART); // TX on GP0
+
+    // I2C Initialization
+    i2c_init(I2C_CHANNEL_ONE, 400 * 1000);  // 400 kHz i2c1 channel
+    gpio_init(IMU_SCL);
+    gpio_init(IMU_SDA);
+    gpio_set_function(IMU_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(IMU_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(IMU_SDA);
+    gpio_pull_up(IMU_SCL);
     
     const uint8_t row_pins[MATRIX_ROWS] = {2, 3, 4, 5};
     const uint8_t col_pins[MATRIX_COLS] = {8, 9};
@@ -45,6 +61,34 @@ int main()
 
     MidiMessenger midi_messenger(uart0);
     ChordController chord_controller(&midi_messenger);
+
+    // IMU Controller Initialization
+    IMU_Controller imu_controller;
+
+    if(imu_controller.init(I2C_CHANNEL_ONE)) {
+
+        // Demo code
+
+        while(true) {
+            // Read vector
+            struct imu_gravity_vector g;
+            imu_controller.readGravityVector(&g);
+
+            // Observationally, corresponds to imu being rotated 90 degrees on one axis
+            if(g.z < 2 && g.z > -2) {
+                gpio_put(LED_PIN, 1);
+            } else {
+                gpio_put(LED_PIN, 0);
+            }
+
+            // imu_controller.debugPrint();
+
+            sleep_ms(100);
+        }
+    }
+
+    gpio_put(LED_PIN, 1);
+
     g_chord_controller = &chord_controller;
 
     fake_strum_irq::FakeStrumIrq fake_strum;
