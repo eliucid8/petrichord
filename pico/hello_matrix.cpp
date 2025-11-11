949e601b23a29b6b21f630df9e7f2a957cdc4eef
Raquel Cervantes <raquelcervantes@Raquels-MacBook-Air.local>
2025-11-10 22:52:52 -0500

    rough pitch detection

--- pico/hello_matrix.cpp
+++ pico/hello_matrix.cpp
@@ -74,7 +74,7 @@ int main()
         auto pr = pitch.update();  //updates mic info
         if(pr.amplitude >= min_sound){
             printf("Pitch frequency: ~%.1f Hz \n",
-            pr.freq_hz, pr.name, pr.amplitude);
+            pr.freq_hz);
         } 
         else{
             printf(" no pitch / too quiet\n");
