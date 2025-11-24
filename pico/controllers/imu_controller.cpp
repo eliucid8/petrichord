ff0e3d22371ef852b74c1918a34a35f322db963d
Sam Ledden <44735694+samledden04@users.noreply.github.com>
2025-11-24 10:41:59 -0500

    Added more robust imu interface

--- pico/controllers/imu_controller.cpp
+++ pico/controllers/imu_controller.cpp
@@ -24,7 +24,7 @@ bool IMU_Controller::init(i2c_inst_t *i2c_port) {
         return false;
     }
 
-    err = bno055_set_operation_mode(BNO055_OPERATION_MODE_ACCGYRO);
+    err = bno055_set_operation_mode(BNO055_OPERATION_MODE_NDOF);
     if(err) {
         printf("IMU_Controller: operation mode request failed.\n");
         return false;
@@ -35,28 +35,77 @@ bool IMU_Controller::init(i2c_inst_t *i2c_port) {
     return true;
 }
 
-bool IMU_Controller::readGravityVector(struct imu_gravity_vector *dst) {
+bool IMU_Controller::readGravityVector(struct imu_xyz_data *dst) {
 
-    struct bno055_accel_float_t accelData;
-    s8 err = bno055_convert_float_accel_xyz_msq(&accelData);
+    bno055_gravity_float_t gravData;
+    s8 err = bno055_convert_float_gravity_xyz_msq(&gravData);
 
     if(err) {
-        printf("IMU_Controller: bno055 data read failiure %d\n", err);
+        printf("IMU_Controller: bno055 gravity read failiure\n");
         return false;
     }
 
-    dst->x = accelData.x;
-    dst->y = accelData.y;
-    dst->z = accelData.z;
+    dst->x = gravData.x;
+    dst->y = gravData.y;
+    dst->z = gravData.z;
+
+    return true;
+}
+
+bool IMU_Controller::readLinearAccelerationVector(struct imu_xyz_data *dst) {
+    bno055_accel_float_t vec;
+    s8 err = bno055_convert_float_accel_xyz_msq(&vec);
+
+    if(err) {
+        printf("IMUController: bno055 linear acceleration read failiure\n");
+        return false;
+    }
+
+    dst->x = vec.x;
+    dst->y = vec.y;
+    dst->z = vec.z;
+
+    return true;
+}
+
+bool IMU_Controller::readEulerCoordinates(struct imu_hrp_data *dst) {
+    bno055_euler_float_t vec;
+    s8 err = bno055_convert_float_euler_hpr_deg(&vec);
+
+    if(err) {
+        printf("IMU Controller: bno055 euler coordinate read failiure\n");
+        return false;
+    }
+
+    dst->h = vec.h;
+    dst->r = vec.r;
+    dst->p = vec.p;
 
     return true;
 }
 
 void IMU_Controller::debugPrint() {
-    struct imu_gravity_vector vec;
+    struct imu_xyz_data grav;
+    struct imu_xyz_data acc;
+    struct imu_hrp_data euler;
 
-    if(readGravityVector(&vec)) {
-        printf("IMU_Controller: X: %.2f, Y: %.2f, Z: %.2f\n", vec.x, vec.y, vec.z);
+    if(readGravityVector(&grav)) {
+        printf("IMU_Controller.readGravityVector: x: %f, y: %f, z: %f\n", grav.x, grav.y, grav.z);
+    } else {
+        printf("IMU_Controller.readGravityVector: read fail\n");
+    }
+
+    if(readEulerCoordinates(&euler)) {
+        printf("IMU_Controller.readEulerCoordinates: h: %f, r: %f, p: %f\n", euler.h, euler.r, euler.p);
+    } else {
+        printf("IMU_Controller.readEulerCoordinates: read fail\n");
+    }
+
+    if(readLinearAccelerationVector(&acc)) {
+        
+        printf("IMU_Controller.readGravityVector: x: %f, y: %f, z: %f\n", acc.x, acc.y, acc.z);
+    } else {
+        printf("IMU_Controller.readGravityVector: read fail\n");
     }
     
 }
--- pico/controllers/imu_controller.h
+++ pico/controllers/imu_controller.h
@@ -4,10 +4,16 @@
 #include "hardware/i2c.h"
 #include "BNO055_driver/bno055.h"
 
-struct imu_gravity_vector {
+struct imu_xyz_data {
     float x;
     float y;
     float z;
+};
+
+struct imu_hrp_data {
+    float h;
+    float r;
+    float p;
 };
 
 class IMU_Controller {
@@ -17,7 +23,11 @@ class IMU_Controller {
         // Initialize BNO055 device and confirm liveness
         bool init(i2c_inst_t *i2c_port);
 
-        bool readGravityVector(struct imu_gravity_vector *dst);
+        bool readGravityVector(struct imu_xyz_data *dst);
+
+        bool readLinearAccelerationVector(struct imu_xyz_data *dst);
+
+        bool readEulerCoordinates(struct imu_hrp_data *dst);
 
         void debugPrint();
     
