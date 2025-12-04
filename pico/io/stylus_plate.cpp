c604ddbfcef2b103c45510dde6db0d2bde15ccf8
Sam Ledden <44735694+samledden04@users.noreply.github.com>
2025-12-04 12:15:23 -0500

    Added simple stylus plate reading, note playing

---
+++ pico/io/stylus_plate.cpp
@@ -0,0 +1,13 @@
+// #include "stylus_plate.h"
+
+// void StylusPlate::eventPinOn(uint pin) {
+//     pin_states_ |= (1 << pin);
+// }
+
+// void StylusPlate::eventPinOff(uint pin) {
+//     pin_states_ &= ~(1 << pin);
+// }
+
+// uint8_t StylusPlate::interpretState() {
+    
+// }
--- pico/petrichord.cpp
+++ pico/petrichord.cpp
@@ -23,6 +23,7 @@ extern "C" {
 #include "controllers/key_matrix_controller.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include <map>
 
 // #include "blink.pio.h"
 
@@ -36,7 +37,7 @@ static ChordController* g_chord_controller = nullptr;
 #define IMU_SCL 27
 
 #define PRINT_AUDIO false
-#define PRINT_IMU false
+#define PRINT_IMU
 #define PRINT_KEYS true
 
 #define CHORD_MATRIX_ROWS 4
@@ -46,9 +47,63 @@ void blink_task(void *pvParameters) {
     int ledState = 1;
 
     while(1) {
+        printf("Blink!\n");
         ledState = ledState ^ 1;
         gpio_put(LED_PIN, ledState);
         vTaskDelay(500);
+    }
+}
+
+
+
+// MUST MATCH PHYSICAL CHARLIEPLEXING STYLE PLATE WIRING
+const std::map<uint8_t, uint8_t> STYLE_PLATE_MAP = {
+    {0b0000011, 0},
+    {0b0000101, 1},
+    {0b0001001, 2},
+    {0b0010001, 3},
+    {0b0100001, 4},
+    {0b1000001, 5},
+    {0b0000110, 6},
+    {0b0001010, 7},
+    {0b0010010, 8},
+    {0b0100010, 9},
+    {0b1000010, 10},
+    {0b0001100, 11},
+    {0b0010100, 12},
+    {0b0100100, 13},
+    {0b1000100, 14},
+    {0b0011000, 15},
+    {0b0101000, 16},
+    {0b1001000, 17},
+    {0b0110000, 18},
+    {0b1010000, 19},
+    {0b1100000, 20}
+};
+
+uint8_t style_plate_state = 0;
+void handle_strum_plate_irq(uint gpio, uint32_t events) {
+    uint8_t pin_bit = 0;
+    switch(gpio) {
+        case 16: pin_bit = 1 << 0; break;
+        case 17: pin_bit = 1 << 1; break;
+        case 19: pin_bit = 1 << 2; break;
+        case 20: pin_bit = 1 << 3; break;
+        case 26: pin_bit = 1 << 4; break;
+        case 27: pin_bit = 1 << 5; break;
+        case 28: pin_bit = 1 << 6; break;
+        default: 
+            printf("WARNING: Unrecognized interrupt pin detected\n");
+            return; // unrecognized pin
+    }
+
+    if(events & GPIO_IRQ_EDGE_RISE) {
+        style_plate_state |= pin_bit;
+    } else if(events & GPIO_IRQ_EDGE_FALL) {
+        style_plate_state &= ~pin_bit;
+    } else {
+        printf("WARNING: Unrecognized interrupt event detected\n");
+        return; // unrecognized event
     }
 }
 
@@ -91,8 +146,6 @@ int main()
     ChordController chord_controller(&midi_messenger);
     KeyMatrixController key_matrix_controller(4, 2, 1, 30);
 
-
-
     // IMU Controller Initialization
     IMU_Controller imu_controller;
     bool imu_status = imu_controller.init(I2C_CHANNEL_ONE);
@@ -100,7 +153,6 @@ int main()
     if(!imu_status) {
         printf("ERROR: imu failed to initialize on i2c1");
     }
-
 
     g_chord_controller = &chord_controller;
 
@@ -128,8 +180,19 @@ int main()
     int demo_imu_state = false;
 
     // FREE RTOS BLINKING TEST
-    xTaskCreate(blink_task, "Blink_LED", 256, NULL, 1, NULL);
-    vTaskStartScheduler( );
+    // xTaskCreate(blink_task, "Blink_LED", 256, NULL, 1, NULL);
+    // vTaskStartScheduler( );
+
+    // Set up interrupts for strum plate
+    gpio_set_irq_enabled_with_callback(16, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &handle_strum_plate_irq);
+    gpio_set_irq_enabled(17, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(19, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(20, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(26, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(27, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(28, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+
+    uint8_t imu_velocity = 127;
 
     while(true) {
         loop_counter++;
@@ -156,6 +219,15 @@ int main()
             auto new_key_state = key_matrix_controller.get_key_state();
             g_chord_controller->update_key_state(new_key_state);
         }
+
+        auto key_selected = STYLE_PLATE_MAP.find(style_plate_state);
+        if(key_selected != STYLE_PLATE_MAP.end()) {
+            // valid style plate state detected
+            uint8_t style_index = key_selected->second;
+            
+            // Get key for specific chord chosen
+            g_chord_controller->handle_strum(style_index);
+        }
         
 
         // =========
@@ -168,23 +240,39 @@ int main()
             struct imu_xyz_data g;
             imu_controller.readGravityVector(&g);
 
-            // Observationally, corresponds to imu being rotated 90 degrees on one axis
-            if(g.z < 2 && g.z > -2) {
-                // gpio_put(LED_PIN, 1);
-                if (!demo_imu_state) {
-                    midi_messenger.send_midi_note_on(Note(36, 120));
-                    demo_imu_state = true;
-                }
+            // Test code: corresponds to imu being rotated 90 degrees on one axis
+            // if(g.z < 2 && g.z > -2) {
+            //     // gpio_put(LED_PIN, 1);
+            //     if (!demo_imu_state) {
+            //         midi_messenger.send_midi_note_on(Note(36, 120));
+            //         demo_imu_state = true;
+            //     }
+            // } else {
+            //     // gpio_put(LED_PIN, 0);
+            //     if (demo_imu_state) {
+            //         midi_messenger.send_midi_note_off(Note(36, 0));
+            //         demo_imu_state = false;
+            //     }
+            // }
+
+            float velocity_float = (std::abs(g.x) / 9.81f) * 127.0f;
+            // convert to uint8_t safely
+            if (velocity_float > 127.0f) {
+                imu_velocity = 127;
+            } else if (velocity_float < 0.0f) {
+                imu_velocity = 0;
             } else {
-                // gpio_put(LED_PIN, 0);
-                if (demo_imu_state) {
-                    midi_messenger.send_midi_note_off(Note(36, 0));
-                    demo_imu_state = false;
-                }
+                imu_velocity = static_cast<uint8_t>(velocity_float);
             }
-            if(PRINT_IMU) {
-                imu_controller.debugPrint();
-            }
+            
+
+            // if(PRINT_IMU) {
+            //     imu_controller.debugPrint();
+            // }
+
+            #ifdef PRINT_IMU
+            printf("IMU Velocity: %d\n", imu_velocity);
+            #endif
         }
         
         // superloop baby!
